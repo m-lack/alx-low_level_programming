81ee317edb3c290394907537c87c3ff9a34bfea4
m-lack <mohieeddin3lakhdar@gmail.com>
2023-10-12 15:04:03 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_prime(long n) {
+    if (n <= 1) {
+        return 0;
+    }
+    
+    if (n <= 3) {
+        return 1;
+    }
+
+    if (n % 2 == 0 || n % 3 == 0) {
+        return 0;
+    }
+
+    for (long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+long largest_prime_factor(long n) {
+    long largest = -1;
+
+    while (n % 2 == 0) {
+        largest = 2;
+        n /= 2;
+    }
+
+    for (long i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            largest = i;
+            n /= i;
+        }
+    }
+
+    if (n > 2) {
+        largest = n;
+    }
+
+    return largest;
+}
+
+int main() {
+    long number = 612852475143;
+    long largest = largest_prime_factor(number);
+    
+    printf("%ld\n", largest);
+
+    return 0;
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9 followed by a new line
+ */
+void print_numbers(void)
+{
+    char c;
+
+    for (c = '0'; c <= '9'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
+}
+
