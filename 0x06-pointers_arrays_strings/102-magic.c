93998cfd9b1bbf4df98fd0e31bf1565de47cacbe
m-lack <mohieeddin3lakhdar@gmail.com>
2023-10-21 09:09:05 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,9 +3,6 @@
 /**
  * main - Entry point
  *
- * Description: This is the main function that performs some magic. It assigns
- * a value to a[2] indirectly through a pointer p and then prints a[2].
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -16,7 +13,17 @@ int *p;
 
 a[2] = 1024;
 p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 *(p + 5) = 98;
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
+
