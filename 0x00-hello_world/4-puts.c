4cecde29cd79fbfde113b69a3ba3c73822df7637
m-lack <mohieeddin3lakhdar@gmail.com>
2023-10-27 04:37:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,7 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+int main() {
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
 }
-
