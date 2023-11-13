0b2a17701332bb066ced3a7fd9562d998e6c68c2
m-lack <mohieeddin3lakhdar@gmail.com>
2023-11-13 03:02:22 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_args - prints all arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: None
+ */
+void print_args(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		_puts(argv[i]);
+		_putchar('\n');
+	}
+}
+
+/**
+ * main - entry point of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	print_args(argc, argv);
+
+	return (0);
+}
+
