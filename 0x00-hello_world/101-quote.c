04c51278040beed1825c7558292467529626a8a7
m-lack <mohieeddin3lakhdar@gmail.com>
2023-11-13 01:41:42 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #include <unistd.h>
 
 int main(void)
@@ -7,4 +5,3 @@ int main(void)
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
-
