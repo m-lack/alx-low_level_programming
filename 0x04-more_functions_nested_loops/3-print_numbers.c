f6f4dceedc9c335f21bc824f627f99eaadb7f368
m-lack <mohieeddin3lakhdar@gmail.com>
2023-11-13 02:18:11 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,24 +1,15 @@
 #include "main.h"
 
 /**
- * _putchar - custom putchar function (declare or define it as needed)
- * @c: the character to print
- *
- * Return: On success, the character is printed, and it returns the character
- *         On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c);
-
-/**
  * print_numbers - prints numbers 0-9 followed by a new line
  */
 void print_numbers(void)
 {
-char c;
+    char c;
 
-for (c = '0'; c <= '9'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+    for (c = '0'; c <= '9'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
 }
