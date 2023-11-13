50165a0af2a57a3b06821b42f18754910da62f70
m-lack <mohieeddin3lakhdar@gmail.com>
2023-11-13 03:24:52 -0800

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,31 @@
 #include "main.h"
 
+/**
+ * print_args - prints all arguments it receives
+ * @argc: the number of arguments
+ * @argv: an array of strings containing the arguments
+ */
+void print_args(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        _puts(argv[i]);
+    }
+}
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
 int main(int argc, char *argv[])
 {
     print_args(argc, argv);
     return 0;
-}
-
-void print_args(int argc, char *argv[])
-{
-    int i;
-
-    for (i = 0; i < argc; i++)
-    {
-        _puts(argv[i]);
-    }
 }
 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,12 +2,9 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <stddef.h>
 
 int _putchar(char c);
 void _puts(char *str);
-void print_args(int argc, char *argv[]);
-ssize_t write(int fildes, const void *buf, size_t nbyte);
 
 #endif /* MAIN_H */
 
