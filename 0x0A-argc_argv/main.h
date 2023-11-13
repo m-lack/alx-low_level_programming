39e473b535f6415d9bf06f3719b192bd409f1243
m-lack <mohieeddin3lakhdar@gmail.com>
2023-11-13 03:10:48 -0800

    2-args.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,5 +6,8 @@ void print_program_name(char *name);
 int count_arguments(int argc, char *argv[]);
 void _puts(char *str);
 
+/* Add the following declaration for write function */
+ssize_t write(int fildes, const void *buf, size_t nbyte);
+
 #endif /* MAIN_H */
 
