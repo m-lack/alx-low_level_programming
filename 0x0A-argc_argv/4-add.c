1a08fe119f904b7795be81c6940e0b16f91d8e1e
m-lack <mohieeddin3lakhdar@gmail.com>
2023-11-20 08:56:24 -0800

    0x12. C - Singly linked lists

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check if string contains only digits
+ * @str: input string
+ *
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int check_num(char *str)
+{
+    unsigned int count = 0;
+
+    while (count < strlen(str))
+    {
+        if (!isdigit(str[count]))
+        {
+            return (0);
+        }
+        count++;
+    }
+    return (1);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+    int count, str_to_int, sum = 0;
+
+    count = 1;
+    while (count < argc)
+    {
+        if (check_num(argv[count]))
+        {
+            str_to_int = atoi(argv[count]);
+            sum += str_to_int;
+        }
+        else
+        {
+            printf("Error\n");
+            return (1);
+        }
+        count++;
+    }
+
+    printf("%d\n", sum);
+
+    return (0);
+}
+
--- README.md
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-"Repository for 0x00. C - Hello, World project in the alx-low_level_programming curriculum."
