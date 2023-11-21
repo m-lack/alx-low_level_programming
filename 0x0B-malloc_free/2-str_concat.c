a3004af37815cc98041373e2bebc87b3d2884854
m-lack <mohieeddin3lakhdar@gmail.com>
2023-11-21 04:20:52 -0800

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,32 +12,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int len, len1, len2, i, j;
-	char *conStr;
+    int len, len1, len2, i, j;
+    char *conStr;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)if (s2 == NULL)
-		s2 = "";
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+    for (len1 = 0; s1[len1] != '\0'; len1++)
+        ;
+    for (len2 = 0; s2[len2] != '\0'; len2++)
+        ;
 
-	len = len1 + len2;
-	conStr = malloc(sizeof(char) * (len + 1));
+    len = len1 + len2;
+    conStr = malloc(sizeof(char) * (len + 1));
 
-	if (conStr == NULL)
-	{
-		free(conStr);
-		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-		conStr[i] = s1[i];
+    if (conStr == NULL)
+    {
+        free(conStr);
+        return (NULL);
+    }
 
-       	j = len2;
-	for (len2 = 0; len2 <= j; i++, len2++)
-		conStr[i] = s2[len2];
-	return (conStr);
-       
+    for (i = 0; i < len1; i++)
+        conStr[i] = s1[i];
+
+    j = 0; // Reset j to 0
+    for (; i < len; i++, j++)
+        conStr[i] = s2[j];
+
+    return (conStr);
+}
+
