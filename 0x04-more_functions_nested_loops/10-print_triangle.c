696656fed6e4b82900e4d9067a50c8e2a0880a89
M-LACK <immlack@gmail.com>
2024-06-28 14:48:39 -0700

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a right-angled triangle of '#' characters in the terminal
- * @size: the size of the triangle
+ * print_triangle - prints a triangle of '#' characters.
+ * @size: size of the triangle.
  */
 void print_triangle(int size)
 {
-    if (size <= 0)
-    {
+    if (size <= 0) {
+        _putchar('\n');
+        return;
+    }
+
+    int row, col, space;
+
+    for (row = 1; row <= size; row++) {
+        // Print spaces
+        for (space = size - row; space > 0; space--) {
+            _putchar(' ');
+        }
+
+        // Print '#' characters
+        for (col = 1; col <= row; col++) {
+            _putchar('#');
+        }
+
+        // Print newline after each row
         _putchar('\n');
     }
-    else
-    {
-        int i, j;
-
-        for (i = 1; i <= size; i++)
-        {
-            for (j = 1; j <= size - i; j++)
-            {
-                _putchar(' ');
-            }
-            for (j = 1; j <= i; j++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
 }
-
