75ab2fcb8e2cbb898d60405079d87e8c7deaef0b
M-LACK <immlack@gmail.com>
2024-07-10 15:55:47 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,34 +1,47 @@
 #include "main.h"
-#include <string.h>
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- * is_palindrome - Check if a string is a palindrome.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- * A palindrome is a word, phrase, number, or other sequence of characters
- * that reads the same forward and backward (ignoring spaces, punctuation,
- * and capitalization).
- *
- * @s: The string to check for palindrome status.
- *
- * Return: 1 if 's' is a palindrome, 0 if not.
+ * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
 {
-    int length = strlen(s);
-    int i, j;
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
 
-    for (i = 0, j = length - 1; i < j; i++, j--) {
-        while (i < j && !isalnum(s[i])) {
-            i++;
-        }
-        while (i < j && !isalnum(s[j])) {
-            j--;
-        }
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
 
-        if (tolower(s[i]) != tolower(s[j])) {
-            return 0;  // Not a palindrome
-        }
-    }
-
-    return 1;  // It's a palindrome
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
