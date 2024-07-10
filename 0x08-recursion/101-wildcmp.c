702097385942ba3b9c44d08cba7c682313f18779
M-LACK <immlack@gmail.com>
2024-07-10 15:57:24 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,34 +1,29 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * is_palindrome - Check if a string is a palindrome.
- *
- * A palindrome is a word, phrase, number, or other sequence of characters
- * that reads the same forward and backward (ignoring spaces, punctuation,
- * and capitalization).
- *
- * @s: The string to check for palindrome status.
- *
- * Return: 1 if 's' is a palindrome, 0 if not.
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-int is_palindrome(char *s)
+
+int wildcmp(char *s1, char *s2)
 {
-    int length = strlen(s);
-    int i, j;
-
-    for (i = 0, j = length - 1; i < j; i++, j--) {
-        while (i < j && !isalnum(s[i])) {
-            i++;
-        }
-        while (i < j && !isalnum(s[j])) {
-            j--;
-        }
-
-        if (tolower(s[i]) != tolower(s[j])) {
-            return 0;  // Not a palindrome
-        }
-    }
-
-    return 1;  // It's a palindrome
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
